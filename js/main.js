720fa6e3fb375532e128bb0ef1327bbaaf08015d
赵开 <zhkai0530@sina.com>
2020-05-31 23:12:50 +0800

    chenge page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(window).ready(function(){
+	/*初始化页面*/
+	$(".page").siblings(".pageOne").show();
+	/*设置页面宽高*/
 	function setBgimgWH(){
 		var $width = $(window).outerWidth();
 		var $height = $(window).height();
@@ -11,4 +14,15 @@ $(window).ready(function(){
 	$(window).resize(function(){
 		setBgimgWH();
 	})
+	/*页面跳转*/
+	$(".nav li").click(function(){
+		$(".page").hide();
+		var index = $(this).index();
+		$(".page").eq(index).show();
+	})
+	function jump(){
+		// 给页面添加类：添加页面消失动画 
+		var $this = $(this);
+		var index = $this.index();
+	}
 })
