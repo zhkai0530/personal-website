32d658e93ed1ff1c9dc712bab7d284e4d6724212
赵开 <zhkai0530@sina.com>
2020-06-03 17:51:00 +0800

    finished of mousewheelEvent

--- js/main.js
+++ js/main.js
@@ -15,23 +15,42 @@ $(window).ready(function(){
 		setBgimgWH();
 	})
 	/*页面跳转*/
+	// 点击跳转
 	$(".nav li").click(function(){
 		$(".page").hide();
 		var index = $(this).index();
 		$(".page").eq(index).show();
 	})
+	// 鼠标滚动跳转
+	function mouseJump(onOff){
+		$(".page").each(function(){
+			if($(this).css("display")=="block"){
+				if(onOff){
+					$index = $(this).index(".page") + 1;
+					if($index >= $(".page").length) $index = $(".page").length - 1;
+				}else{
+					$index = $(this).index(".page") - 1;
+					if($index < 0) $index = 0;
+				}
+			}
+		})
+		$(".page").css("display","none");
+		$(".page").eq($index).css("display","block");
+	}
 	$(window).on("mousewheel",function(event){
 		var e = event || window.event;
+		var onOff = false;
+		var $index;
 		var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome & ie
 		
 		          (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));              // firefox
 		
 		if (delta > 0) {
-			console.log("wheelup");
+			onOff = false;
+			mouseJump(onOff);
 		} else if (delta < 0) {
-			if($(".page").show()){
-				console.log($(this).index())
-			}
+			onOff = true;
+			mouseJump(onOff);
 		}
 	})
 	function jump(){
