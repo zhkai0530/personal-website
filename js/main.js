3a38e3deb04b8c587c835de208a0a2fa5c6012c3
赵开 <zhkai0530@sina.com>
2020-06-17 17:29:40 +0800

    优化

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 $(window).ready(function(){
+	
+	/*封装函数--获取可视页面宽高*/
+	function getSize(){
+		var whValue = new Object;
+		whValue.$width = $(window).outerWidth();
+		whValue.$height = $(window).height();
+		return whValue;
+	}
+	
 	/*初始化页面*/
 	$(".page").siblings(".pageOne").show();
+	
 	/*设置页面宽高*/
 	function setBgimgWH(){
-		var $width = $(window).outerWidth();
-		var $height = $(window).height();
-		$("body").width($width);
-		$("body").height($height);		
-		$(".page").width($width);
-		$(".page").height($height);
+		var widHei = getSize();
+		$("body").width(widHei.$width);
+		$("body").height(widHei.$height);		
+		$(".page").width(widHei.$width);
+		$(".page").height(widHei.$height);
 	}
 	setBgimgWH();
 	$(window).resize(function(){
 		setBgimgWH();
 	})
+	
 	/*封装函数-页面跳转*/
 	function mouseJump(className,className2,obj,onOff,event){
 		var e = window.event || event;
@@ -46,13 +56,13 @@ $(window).ready(function(){
 			$this.addClass("active");
 		}
 	}
+	
 	// 点击跳转
 	$(".nav li").on("click",function(){
-		mouseJump(".nav li",".page",this)
-		
+		mouseJump(".nav li",".page",this);
 	})
+	
 	// 鼠标滚动跳转
-
 	$(window).on("mousewheel",function(event){
 		var e = event || window.event;
 		var onOff = false;
@@ -68,5 +78,55 @@ $(window).ready(function(){
 			mouseJump(".page",".nav li",this,onOff);
 		}
 	})
+	
+/*作品展示页*/
+/*-*/
+	$("img").on("click",function(){
+		var widHei = getSize();
+		$(this).css({position:"relative",margin:"0 auto"}).animate({
+			// top:widHei.$height * -0.25 + "px",
+			// left:widHei.$width * -0.2 + "px",
+			// // height:widHei.$height * 0.5 + "px",
+			width:widHei.$width * 0.7 + "px"
+		},1000,function(){})
+	})
+	
+	/*
+	* 宽1600px
+	* 字体大小：4*4=16px
+	* 一行字的数量：100个
+	* rem单位：1rem
+	* html字号：16px
 
+	* 设计宽度980px->1200px
+	* 字体大小24px
+	* 一行字的数量40
+	* rem单位1.5rem
+	* html字号16px -> 30px
+
+	* 1200/40/1.5
+
+	1、获取页面宽度
+	2、计算一行字的数量
+	3、默认字体大小16px
+	
+	function getSize(){
+	    var obj = new Object;
+	    obj.$width = document.body.clientWidth;
+	    obj.$height = document.body.clientHeight;
+	    return obj;
+	}
+	function setFontSize(){
+	    var obj = getSize();
+	    var fontShuliang = 40;
+	    var fontSizeRem = obj.$width / fontShuliang / 16;
+	    var fontSize = obj.$width / fontShuliang / fontSizeRem;
+	    var htmlFS = document.getElementsByTagName("html")[0];
+	    htmlFS.style.fontSize = fontSize;
+	}
+	window.onresize = function(){
+	    setFontSize();
+	}
+	
+	*/
 })
